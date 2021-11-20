4655f3a18187b532ecdf47489651f0f1f473326e
stormsuresh92 <38948398+stormsuresh92@users.noreply.github.com>
2021-11-20 15:04:31 +0530

    Update prnewswire.py

--- prnewswire.py
+++ prnewswire.py
@@ -1,5 +1,7 @@
 from requests_html import HTMLSession
 import pandas as pd
+from alive_progress import alive_bar
+from time import sleep
 
 
 data = []
@@ -8,21 +10,35 @@ def page(x):
     url = f'https://www.prnewswire.com/news-releases/news-releases-list/?page={x}&pagesize=100'
     r = s.get(url)
     content = r.html.find('div.row.arabiclistingcards')
-    for item in content:
-        date_time = item.find('h3', first=True).text.split('ET')[-2]
-        title = item.find('h3', first=True).text.split('ET')[-1]
-        url = 'https://www.prnewswire.com' + item.find('a.newsreleaseconsolidatelink', first=True).attrs['href']
-        dic = {
-            'Date_Time':date_time,
-            'Title':title,
-            'Url':url
-        }
-        data.append(dic)
-    return data
+    with alive_bar(len(content), title=f'Getting page {x}', bar='classic2', spinner='classic') as bar:
+        for item in content:
+            try:
+                date_time = item.find('h3', first=True).text.split('ET')[-2]
+            except:
+                date_time = ''
+            try:
+                title = item.find('h3', first=True).text.split('ET')[-1]
+            except:
+                title = ''
+            try:
+                url = 'https://www.prnewswire.com' + item.find('a.newsreleaseconsolidatelink', first=True).attrs['href']
+            except:
+                url = ''
+            dic = {
+                'Date_Time':date_time,
+                'Title':title,
+                'Urls':url
+            }
+            data.append(dic)
+            sleep(0.2)
+            bar()
 
-for x in range(1, 4):
+endpage = int(input('Enter end page:'))
+for x in range(1, endpage):
     page(x)
     
 df = pd.DataFrame(data)
-df.to_csv('output.csv', index=False)
+df.to_csv('Outputfile.csv', index=False)
+print('\n')
 print('file downloaded') 
+input()
